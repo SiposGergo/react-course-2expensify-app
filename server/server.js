1200051fa58ef592c506285040ce0a64b8f829d9
gergo sipos <prosipinho@gmail.com>
2017-11-25 11:30:19 +0100

    heroku

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendfile(path.join(publicPath, "index.html"));
+});
+
+app.listen(PORT, () => { console.log("server is up") });
